4a9aec08d023715617d3749facca19a3fd25c373
Al Muhdil Karim <almuhdilkarim2@gmail.com>
2025-11-23 22:57:37 +0700

    update

--- prep.sh
+++ prep.sh
@@ -29,7 +29,7 @@ mount -o uid=0,gid=0,dmask=007,fmask=007 $DISKBOOT /mnt/boot/ &&
 mkdir -p /mnt/home &&
 mount /dev/mapper/data /mnt/home &&
 
-pacstrap /mnt  &&
+pacstrap /mnt $DISTRO_INSTALLATION_PACKAGE &&
 
 genfstab -U /mnt > /mnt/etc/fstab &&
 cp -fr /install/post /mnt &&
